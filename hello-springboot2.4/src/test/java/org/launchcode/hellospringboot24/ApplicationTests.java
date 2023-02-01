b3d4019ab084af7bb2a897b61b74552648c2af68
cwc0202 <107936539+cwc0202@users.noreply.github.com>
2023-02-01 14:50:10 -0600

    First commit

---
+++ hello-springboot2.4/src/test/java/org/launchcode/hellospringboot24/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.hellospringboot24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
